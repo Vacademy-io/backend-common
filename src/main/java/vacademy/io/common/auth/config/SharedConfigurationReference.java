8c0ac33d311d6f8b6fc0fd2cbc0912054c9cba85
Shreyash <shreyashjain1996@gmail.com>
2024-10-18 09:43:33 +0530

    added first commit

---
+++ src/main/java/vacademy/io/common/auth/config/SharedConfigurationReference.java
@@ -0,0 +1,14 @@
+package vacademy.io.common.auth.config;
+
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ComponentScan("labor.link.*")
+@EnableJpaRepositories("labor.link.*")
+@EntityScan("labor.link.*")
+public class SharedConfigurationReference {
+}
